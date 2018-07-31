20e36b089239cfe3766e2b53be002b10595e88c4
zhangyi <yi.zhang@shenmajr.com>
2018-07-31 11:38:13 +0800

    udpate

---
+++ src/main/java/com/neal/blog/blogdocument/BlogDocumentApplication.java
@@ -0,0 +1,12 @@
+package com.neal.blog.blogdocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogDocumentApplication.class, args);
+	}
+}
